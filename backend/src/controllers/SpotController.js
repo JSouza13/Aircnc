b5fdb723979054ae8f1f535896eef0e798996536
joao.souza <joao.souza@itajai.quay.com.br>
2019-10-11 17:15:02 -0300

    metodo store para Spots

---
+++ backend/src/controllers/SpotController.js
@@ -0,0 +1,27 @@
+const Spot = require('../models/Spot');
+const User = require('../models/User');
+
+module.exports = {
+  async store(req, res) {
+    const { filename } = req.file;
+    const { company, techs, price } = req.body;
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+
+    if (!user) {
+      return res.status(400).json({ error: 'O usuário não existe' });
+    }
+
+    const spot = await Spot.create({
+      user: user_id,
+      thumbnail: filename,
+      company,
+      // divide a cada "," e remove espaço do inicio e fim de cada string
+      techs: techs.split(',').map(tech => tech.trim()),
+      price,
+    });
+
+    return res.json(spot);
+  },
+};
