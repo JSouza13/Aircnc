9591a2c6be67171555d4774a8e3718043d0fcf72
joao.souza <joao.souza@itajai.quay.com.br>
2019-10-17 17:06:35 -0300

    finalizado Login e rotas da aplciação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,25 +3,15 @@ import "./App.css";
 
 import logo from "./assets/logo.svg";
 
+import Routes from "./routes";
+
 function App() {
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
 
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre{" "}
-          <strong>talentos</strong> para sua empresa
-        </p>
-
-        <form>
-          <label html="email">E-mail *</label>
-          <input type="email" id="email" placeholder="Seu melhor e-mail" />
-
-          <button className="btn" type="submit">
-            Entrar
-          </button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
